adce3637319fec24ccbbd022ed2aa343c3ac4b46
Emmanuel S <5435148+esgn@users.noreply.github.com>
2020-02-07 15:32:18 +0100

    Update renew-deploy.sh

--- manual-auth-hook-example/renew-deploy.sh
+++ manual-auth-hook-example/renew-deploy.sh
@@ -4,6 +4,6 @@ CERTBOT_DIR="/opt/certbot/"
 SCRIPTS_DIR="/home/scripts/certbot"
 
 cd $CERTBOT_DIR
-sudo ./certbot-auto certonly --force-renew -t -d domain.com --manual --manual-auth-hook $SCRIPTS_DIR/certbot_custom_auth.sh --manual-public-ip-logging-ok
+sudo ./certbot-auto certonly --force-renew -t -d domain.com --manual --manual-auth-hook $SCRIPTS_DIR/certbot-custom-auth.sh --manual-public-ip-logging-ok
 cd $SCRIPTS_DIR
 sudo ./copy-ssl-certs-to-dmz.sh
